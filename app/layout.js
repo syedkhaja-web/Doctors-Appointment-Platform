fc5c403c44b37ee375c6af8cf0cf46ad47b31fe7
taqhi <syedtaqhi7557@gmail.com>
2026-01-09 21:44:00 +0530

    fix: align Next.js version with Clerk

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
                 <p>
-                  Made with 💗 by Syed Khaja Taqhi uddin and Mohammed faisal
+                  Made with 💗 by Stark industries
                 </p>
               </div>
             </footer>
